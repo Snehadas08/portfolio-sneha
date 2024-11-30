1521e3642b308b7adb011cf6411ce0769f1d37da
Snehadas08 <snehadas0802@gmail.com>
2024-11-30 18:39:02 +0530

    js file and other files have been changed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,55 @@
-$(function () {
-  // timelines init
-  $("#vt2").verticalTimeline({
-    startLeft: false,
-    arrows: false,
+(function () {
+    const header = document.querySelector(".navbar");
+    const about = document.querySelector(".about-me");
+    // const toggleClass = "is-sticky";
+    let aboutTop = about.offsetTop;
+    console.log(aboutTop);
+    window.addEventListener("scroll", function () {
+      if (window.pageYOffset >= aboutTop) {
+        header.classList.add("navbar-sticky");
+      } else {
+        header.classList.remove("navbar-sticky");
+      }
+    });
+  })();
+  
+  $(document).ready(function () {
+    // scroll to inner sections of a page.
+    (function () {
+      $(".scrollTo").click(function () {
+        // this is a getter variable.
+        // We're getting the current value of the href attribute of each element with a class "scrollTo".
+        var getElem = $(this).attr("href");
+        // This is a variable containing an integer value.
+        var targetDistance = 0;
+        // Checking If the element exists on the page or not.
+        if ($(getElem).length) {
+          // getting the top offset of the targetted element.
+          var getOffset = $(getElem).offset().top;
+          // Now We can animate to our desired section using the html element.
+          $("html").animate(
+            {
+              scrollTop: getOffset - targetDistance,
+            },
+            500
+          );
+        }
+        // returning false performs three tasks when called:
+        // event.preventDefault();
+        // event.stopPropagation();
+        // Stops callback execution and returns immediately when called.
+        return false;
+      });
+      
+    })();
+  
+    $("#vt2").verticalTimeline({
+      startLeft: false,
+      arrows: false,
+    });
+    $("#vt3").verticalTimeline({
+      startLeft: false,
+      arrows: false,
+    });
   });
-  $("#vt3").verticalTimeline({
-    startLeft: false,
-    arrows: false,
-  });
-});
+  
