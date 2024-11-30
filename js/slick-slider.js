ed1bd4f0ef891ee91572d898c70771bca2248896
Snehadas08 <snehadas0802@gmail.com>
2024-11-30 16:56:03 +0530

    html slick slider desktopstyle responsive files updated

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -1,28 +1,31 @@
-$(document).ready(function(){
-    $('.slick-carousel').slick({
-      arrows: true,
-      centerPadding: "0px",
-      dots: true,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      pauseOnHover: false,
-      infinite: true,
-        responsive: [{
-        breakpoint: 768,
+$(document).ready(function () {
+  $(".skills-carousel").slick({
+    arrows: true,
+    centerPadding: "0px",
+    dots: true,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnHover: false,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 769,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          infinite: true
-        }
-      }, {
-        breakpoint: 320,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 500,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          dots: true
-        }
-      }]
-    });
+          dots: false,
+        },
+      },
+    ],
   });
+});
